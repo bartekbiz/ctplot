0a099c5b87f83d85dcad5d2fb205654eb0146a17
Mateusz Grochowski <groszek20027@gmail.com>
2024-04-04 14:25:09 +0200

    Added individual labels for graphs

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 import tkinter as tk
 from tkinter import filedialog
 import csv
-from matplotlib.figure import Figure 
+from matplotlib.figure import Figure
 import matplotlib.pyplot as plt
-from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg,  
-NavigationToolbar2Tk) 
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
 
 
 class App(tk.Tk):
@@ -45,39 +44,40 @@ class App(tk.Tk):
         plt.grid(True)
         # plt.show()
 
-    def plot(self): 
-    
-        # the figure that will contain the plot 
-        fig = Figure() 
-    
-        # adding the subplot 
+    def plot(self):
+
+        # the figure that will contain the plot
+        fig = Figure()
+
+        # adding the subplot
         fig, (ax1, ax2, ax3) = plt.subplots(3, 1, constrained_layout=True)
         ax1.plot(self.data["x"], self.data["y"])
         ax2.plot(self.data["x"], self.data["y"])
         ax3.plot(self.data["x"], self.data["y"])
 
         # separate subplot titles
-        ax1.set_title('Wykres x(t)')
-        ax2.set_title('Wykres v(t)')
-        ax3.set_title('Wykres a(t)')
+        ax1.set_title("Wykres x(t)")
+        ax2.set_title("Wykres v(t)")
+        ax3.set_title("Wykres a(t)")
+
+        ax1.set_ylabel("x [m]")
+        ax2.set_ylabel("x [m]")
+        ax3.set_ylabel("x [m]")
 
         # common axis labels
-        fig.supxlabel('t [s]')
-        fig.supylabel('x [m]')
-    
-        # creating the Tkinter canvas 
-        # containing the Matplotlib figure 
-        canvas = FigureCanvasTkAgg(fig, 
-                                master = self)   
-        canvas.draw() 
-    
-        # placing the canvas on the Tkinter window 
-        canvas.get_tk_widget().pack() 
-    
-        # creating the Matplotlib toolbar 
-        toolbar = NavigationToolbar2Tk(canvas, 
-                                    self) 
-        toolbar.update() 
-    
-        # placing the toolbar on the Tkinter window 
-        canvas.get_tk_widget().pack() 
+        fig.supxlabel("t [s]")
+
+        # creating the Tkinter canvas
+        # containing the Matplotlib figure
+        canvas = FigureCanvasTkAgg(fig, master=self)
+        canvas.draw()
+
+        # placing the canvas on the Tkinter window
+        canvas.get_tk_widget().pack()
+
+        # creating the Matplotlib toolbar
+        toolbar = NavigationToolbar2Tk(canvas, self)
+        toolbar.update()
+
+        # placing the toolbar on the Tkinter window
+        canvas.get_tk_widget().pack()

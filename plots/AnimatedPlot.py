8ffb70bd3ee952b0e1d7eec0d285a7472d3d5416
Bartosz Bizoń <bartosz.bizon17@gmail.com>
2024-04-15 15:06:46 +0200

    Implemented refreshing data for v and a plots

--- plots/AnimatedPlot.py
+++ plots/AnimatedPlot.py
@@ -1,47 +1,95 @@
 from matplotlib import pyplot as plt
 from matplotlib.animation import FuncAnimation
+import tkinter as tk
 from plots.MainPlot import MainPlot
 from plots.PlotCalculations import PlotCalculations
+
 
 class AnimatedPlot(MainPlot):
     def __init__(self, app):
         super().__init__(app)
 
+        self.animation = None
         self.animated_x = []
         self.animated_y = []
+
         self.counter = 0
+        self.refresh_rate = 30
 
-    def create_figure(self):
-        # adding the subplot
-        self.fig, (self.ax1, self.ax2, self.ax3) = plt.subplots(3, 1, constrained_layout=True)
-        self.fig.set_figwidth(6.6)
-        self.fig.set_figheight(6.6)
+    def create_plot(self):
+        print("\nCreating plot...")
+        self.close_plot()
 
-        self.set_single_plot_props(self.ax1, "Wykres x(t)", "x [m]", self.animated_x, self.animated_y)
+        self.create_figure()
 
-        plot_calculations = PlotCalculations()
+        self.set_plots_props()
 
-        v_x, v_y = plot_calculations.calc_linear_regression(self.animated_x, self.animated_y, 10)
-        self.set_single_plot_props(self.ax2, "Wykres v(t)", "v [m/s]", v_x, v_y)
+        self.update_ax1_data()
+        self.update_ax2_ax3_data()
 
-        a_x, a_y = plot_calculations.calc_linear_regression(v_x, v_y, 10)
-        self.set_single_plot_props(self.ax3, "Wykres a(t)", "a [m/s*s]", a_x, a_y)
+        self.set_min_max_values()
+
+        self.create_canvas()
+        self.create_toolbar()
 
         self.animation = FuncAnimation(self.fig, self.update_frame, interval=5)
 
-        # common axis labels
+        self.update_close_button_state(tk.NORMAL)
+
+    def create_figure(self):
+        self.fig, (self.ax1, self.ax2, self.ax3) = plt.subplots(3, 1, constrained_layout=True)
+
+        self.fig.set_figwidth(6.6)
+        self.fig.set_figheight(6.6)
+
         self.fig.supxlabel("t [s]")
+
+    def set_plots_props(self):
+        self.set_single_plot_props(self.ax1, "Wykres x(t)", "x [m]")
+        self.set_single_plot_props(self.ax2, "Wykres v(t)", "v [m/s]")
+        self.set_single_plot_props(self.ax3, "Wykres a(t)", "a [m/s*s]")
+
+    @staticmethod
+    def set_single_plot_props(ax: plt.axes, title, y_label):
+        ax.set_title(title)
+        ax.set_ylabel(y_label)
+        ax.grid()
+
+    def update_ax1_data(self):
+        self.ax1.plot(self.animated_x, self.animated_y)
+
+    def update_ax2_ax3_data(self):
+        plot_calculations = PlotCalculations()
+
+        v_x, v_y = plot_calculations.calc_linear_regression(self.animated_x, self.animated_y, self.custom_span.get())
+        a_x, a_y = plot_calculations.calc_linear_regression(v_x, v_y, self.custom_span.get())
+
+        self.ax2.plot(v_x, v_y)
+        self.ax3.plot(a_x, a_y)
 
     def update_frame(self, frame):
         self.animated_x.append(self.app.data["x"][self.counter])
         self.animated_y.append(self.app.data["y"][self.counter])
+
+        self.update_ax1_data()
+        if self.counter % self.refresh_rate == 0:
+            self.update_ax2_ax3_data()
+
         self.counter += 1
-        self.ax1.plot(self.animated_x, self.animated_y)
-        return self.ax1
-        
-    @staticmethod
-    def set_single_plot_props(ax, title, y_label, x_data, y_data):
-        ax.plot(x_data, y_data)
-        ax.set_title(title)
-        ax.set_ylabel(y_label)
-        ax.grid()
+
+        return self.ax1, self.ax2, self.ax3
+
+    def close_plot(self):
+        if self.canvas is None and self.toolbar is None:
+            return
+
+        print("Closing plot...")
+        self.canvas.get_tk_widget().destroy()
+        self.toolbar.destroy()
+        self.canvas = None
+        self.toolbar = None
+
+        self.animated_x = []
+        self.animated_y = []
+
+        self.update_close_button_state(tk.DISABLED)

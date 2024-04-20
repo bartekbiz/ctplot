859729f1966ecc4cec1bcf218baba1d7126f4bbf
Vladyslav Dikhtiaruk <vlad.dikhtiaruk@gmail.com>
2024-04-20 16:59:48 +0200

    Added row parameter for ApplyButton and SpanField

--- controls/ApplyButton.py
+++ controls/ApplyButton.py
@@ -2,13 +2,13 @@ from controls.base.SmallButton import SmallButton
 
 
 class ApplyButton(SmallButton):
-    def __init__(self, module):
+    def __init__(self, module, row):
         super().__init__(
             module.app,
             text="Apply",
             command=module.apply
         )
-        self.grid(row=9, column=1, padx=10, sticky="ne")
+        self.grid(row=row, column=1, padx=10, sticky="ne")
 
         # Binding Enter key to apply button
         self.bind("<Return>", module.apply)

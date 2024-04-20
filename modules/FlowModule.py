ae6c281963791f76c53f5ceb9a5be971d30a63d1
Mateusz Grochowski <groszek20027@gmail.com>
2024-04-20 13:33:20 +0200

    Added cross section field

--- modules/FlowModule.py
+++ modules/FlowModule.py
@@ -1,4 +1,5 @@
 from controls.FlowModule.DiameterField import DiameterField
+from controls.FlowModule.CrossSectionField import CrossSectionField
 from modules.BaseModule import BaseModule
 from enums.ModuleEnum import ModuleEnum
 
@@ -7,6 +8,7 @@ class FlowModule(BaseModule):
     def __init__(self, app):
         super().__init__(app)
         self.diameter_field = DiameterField(self)
+        self.cross_section_field = CrossSectionField(self)
 
     def get_name(self):
         return ModuleEnum.flow

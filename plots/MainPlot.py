193b5e53588e6ce00d1d35487428ce2cb81d9377
Vladyslav Dikhtiaruk <vlad.dikhtiaruk@gmail.com>
2024-04-21 13:53:02 +0200

    Refactor of MainPlot

--- plots/MainPlot.py
+++ plots/MainPlot.py
@@ -42,76 +42,16 @@ class MainPlot:
         self.set_span_value()
 
     def set_min_max_values(self):
-        # X min/max for the 1 plot
-        x_min = self.x_min.get()
-        x_max = self.x_max.get()
-        self.set_x_min_max_values_ax1(x_min, x_max)
+        self.set_axis_limits(self.ax1, self.x_min.get(), self.x_max.get(), self.y_min.get(), self.y_max.get())
+        self.set_axis_limits(self.ax2, self.x_min_2.get(), self.x_max_2.get(), self.y_min_2.get(), self.y_max_2.get())
+        self.set_axis_limits(self.ax3, self.x_min_3.get(), self.x_max_3.get(), self.y_min_3.get(), self.y_max_3.get())
 
-        # Y min/max for the 1 plot
-        y_min = self.y_min.get()
-        y_max = self.y_max.get()
-        self.set_y_min_max_values_ax1(y_min, y_max)
-
-
-        # X min/max for the 2 plot
-        x_min_2 = self.x_min_2.get()
-        x_max_2 = self.x_max_2.get()
-        self.set_x_min_max_values_ax2(x_min_2, x_max_2)
-
-        # Y min/max for the 2 plot
-        y_min_2 = self.y_min_2.get()
-        y_max_2 = self.y_max_2.get()
-        self.set_y_min_max_values_ax2(y_min_2, y_max_2)
-
-        # X min/max for the 3 plot
-        x_min_3 = self.x_min_3.get()
-        x_max_3 = self.x_max_3.get()
-        self.set_x_min_max_values_ax3(x_min_3, x_max_3)
-
-        # Y min/max for the 3 plot
-        y_min_3 = self.y_min_3.get()
-        y_max_3 = self.y_max_3.get()
-        self.set_y_min_max_values_ax3(y_min_3, y_max_3)
-
-
-
-
-    # setting x min/max values
-    def set_x_min_max_values_ax1(self, x_min, x_max):
+    def set_axis_limits(self, axis, x_min, x_max, y_min, y_max):
         if x_min < x_max:
-            self.ax1.set_xlim(left=x_min, right=x_max)
-
-    def set_x_min_max_values_ax2(self, x_min_2, x_max_2):
-        if x_min_2 < x_max_2:
-            self.ax2.set_xlim(left=x_min_2, right=x_max_2)
-
-    def set_x_min_max_values_ax3(self, x_min_3, x_max_3):
-        if x_min_3 < x_max_3:
-            self.ax3.set_xlim(left=x_min_3, right=x_max_3)
-
-    # setting y min/max values
-    def set_y_min_max_values_ax1(self, y_min, y_max):
+            axis.set_xlim(left=x_min, right=x_max)
         if y_min < y_max:
-            self.ax1.set_ylim(bottom=y_min, top=y_max)
-
-    def set_y_min_max_values_ax2(self, y_min_2, y_max_2):
-        if y_min_2 < y_max_2:
-            self.ax2.set_ylim(bottom=y_min_2, top=y_max_2)
-
-    def set_y_min_max_values_ax3(self, y_min_3, y_max_3):
-        if y_min_3 < y_max_3:
-            self.ax3.set_ylim(bottom=y_min_3, top=y_max_3)
-
-
-    # def set_x_min_max_values(self, x_min, x_max):
-    #     self.ax1.set_xlim(left=x_min, right=x_max)
-    #     self.ax2.set_xlim(left=x_min, right=x_max)
-    #     self.ax3.set_xlim(left=x_min, right=x_max)
-
-    # def set_y_min_max_values(self, y_min, y_max):
-    #     self.ax1.set_ylim(bottom=y_min, top=y_max)
-    #     self.ax2.set_ylim(bottom=y_min, top=y_max)
-    #     self.ax3.set_ylim(bottom=y_min, top=y_max)
+            axis.set_ylim(bottom=y_min, top=y_max)
+        print(f"Setting x and y limits for {axis}:x({x_min}, {x_max}), y({y_min}, {y_max}")
 
     def set_span_value(self):
         if self.custom_span.get() < 1:

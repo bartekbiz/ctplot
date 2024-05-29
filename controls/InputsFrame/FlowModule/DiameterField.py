91b15040c03e9186e62a10c023ac36561641f478
Mateusz Grochowski <groszek20027@gmail.com>
2024-05-29 12:48:40 +0200

    Added m to diamater

--- controls/InputsFrame/FlowModule/DiameterField.py
+++ controls/InputsFrame/FlowModule/DiameterField.py
@@ -14,7 +14,7 @@ class DiameterField:
 
 class DiameterLabel(TextLabel):
     def __init__(self, window, row):
-        super().__init__(window, text="Diameter")
+        super().__init__(window, text="Diameter [mm]")
         self.grid(row=row, column=0, padx=10, sticky="nw")
 
 
